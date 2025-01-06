5dd80e1f3adfab7d0d4151a92dd4407166e23ef2
Brandy Wyles <brandy.wyles@accenture.com>
2025-01-06 16:49:56 -0500

    changes from video 6

---
+++ src/main/java/com/telusko/DemoApp/Dev.java
@@ -0,0 +1,12 @@
+package com.telusko.DemoApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void build(){
+        System.out.println("Working on an Awesome Project");
+    }
+
+}
