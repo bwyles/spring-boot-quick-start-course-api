8d6521f9c59acfd2092a34468cacd200936734ca
Brandy Wyles <brandy.wyles@accenture.com>
2025-01-17 17:24:18 -0500

    day 10: microservices 5-7

--- src/main/java/io/javabrains/springbootstarter/controller/GreetingController.java
+++ src/main/java/io/javabrains/springbootstarter/controller/GreetingController.java
@@ -1,17 +1,30 @@
 package io.javabrains.springbootstarter.controller;
 
+import io.javabrains.springbootstarter.configuration.DbSettings;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class GreetingController {
 
-    @Value("${my.greeting}")
+    @Value("${my.greeting: default value}")
     private String greetingMessage;
 
-    @RequestMapping("/greeting")
+    @Value("some static message")
+    private String staticMessage;
+
+    @Value("${my.list.values}")
+    private List<String> listValues;
+
+    @Autowired
+    private DbSettings dbSettings;
+
+    @GetMapping("/greeting")
     public String greeting(){
-        return greetingMessage;
+        return dbSettings.getConnection() + " " + dbSettings.getHost() + " " + dbSettings.getPort();
     }
 }
